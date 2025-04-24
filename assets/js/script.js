5ebef042387efa8df44c0f6768c05bb01bb499ce
jhowzluk <jhonatanluksik@gmail.com>
2025-04-23 21:52:48 -0300

    feat: validação do campo nome

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,10 @@
-window.addEventListener("scroll", function () {
-    const button = document.getElementById("back-to-top");
-    if (window.scrollY > 200) { // Exibe o botão após 200px de rolagem
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-});
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth" // Rola suavemente
-    });
+
+function validaFaleConosco() {
+    console.log("validaFaleConosco");
+    if (document.getElementById("name").value == "") {
+        alert("Preencha o campo Nome.");
+        document.getElementById("name").focus();
+        return false;
+    }
 }
